977e0f8db78ec235e518cf830849e55a1e39f879
jagjeet0824 <jagjeet0824@gmail.com>
2018-04-27 14:44:59 -0400

    first commit

---
+++ Models/EFBreweryRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace comp2007w2018finalB.Models
+{
+    public class EFBreweryRepository : IMockBreweryRepository
+    {
+        // database connection
+        private CraftBrewingModel db = new CraftBrewingModel();
+
+        public IQueryable<Brewery> Breweries { get { return db.Breweries; } }
+
+        public void Save(Brewery brewery)
+        {
+            if (brewery.BreweryId == 0)
+            {
+                db.Breweries.Add(brewery);
+            }
+            else
+            {
+                db.Entry(brewery).State = System.Data.Entity.EntityState.Modified;
+            }
+
+            db.SaveChanges();
+        }
+    }
+}
